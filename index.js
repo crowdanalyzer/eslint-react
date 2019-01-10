f9a89695f0b32fcf4f73d9db3112abb9e726c8fa
Maha Zaid <maha.raafatt@gmail.com>
2019-01-10 12:46:03 +0200

    Add eslint react rules (#1)

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-'use strict'
+/* eslint quotes: ['off'] */
+'use strict';
 
-module.exports = {};
+module.exports = {
+    extends: [
+        './rules/jsx',
+        './rules/react',
+        './rules/stylistic-issues',
+    ].map(require.resolve),
+
+    parserOptions: {
+        ecmaVersion: 8,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+
+    parser: 'babel-eslint',
+
+    plugins: ['react'],
+
+    env: {
+        node: true,
+        es6: true,
+        mocha: true,
+    },
+};
---
+++ rules/stylistic-issues.js
@@ -0,0 +1,18 @@
+/* eslint quotes: ['off'] */
+'use strict';
+
+module.exports = {
+    rules: {
+        // keywords spacing
+        'keyword-spacing': ['error', {
+            overrides: {
+                from: { after: true },
+                default: { after: true },
+                if: { after: false },
+            },
+        }],
+
+        // Use double quotes for strings
+        quotes: ['error', 'double', { avoidEscape: true, allowTemplateLiterals: true }],
+    },
+};
