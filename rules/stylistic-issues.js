c2c2d49944b41e5415e84e107504ee4ac6f7b0ce
ali <ali.hassan.abdallatif@gmail.com>
2019-05-21 13:14:55 +0200

    add the 2 spaces rule

--- rules/stylistic-issues.js
+++ rules/stylistic-issues.js
@@ -1,18 +1,24 @@
 /* eslint quotes: ['off'] */
-'use strict';
+"use strict";
 
 module.exports = {
-    rules: {
-        // keywords spacing
-        'keyword-spacing': ['error', {
-            overrides: {
-                from: { after: true },
-                default: { after: true },
-                if: { after: false },
-            },
-        }],
+  rules: {
+    // keywords spacing
+    "keyword-spacing": [
+      "error",
+      {
+        overrides: {
+          from: { after: true },
+          default: { after: true },
+          if: { after: false },
+        },
+      },
+    ],
 
-        // Use double quotes for strings
-        quotes: ['error', 'double', { avoidEscape: true, allowTemplateLiterals: true }],
-    },
+    // Use double quotes for strings
+    quotes: ["error", "double", { avoidEscape: true, allowTemplateLiterals: true }],
+
+    // use a consistent indentation style of 2 spaces.
+    indent: ["error", 2],
+  },
 };
