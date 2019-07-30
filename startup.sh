263ea7f1ec3ada9dd049011d301521f2a38e483b
ShadyMohammed <shadymohammed93@gmail.com>
2019-07-30 11:50:19 +0200

    remove jsx-no-bind rule and add .tsx to accepted file extensions for jsx

--- startup.sh
+++ startup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-echo "Copying .npmrc-example into .npmrc"
-cp /var/code/.npmrc-example /var/code/.npmrc
-
-echo "Removing node_modules"
-rm -r node_modules
-  
 echo "Installing dependencies"
 npm install
-  
+
 echo "Startup commands are completed successfully"
 bash
